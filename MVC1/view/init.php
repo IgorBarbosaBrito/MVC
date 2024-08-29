016f960524ca674ba4d46fb50e319bc3b2388df1
IgorBarbosaBrito <151582506+IgorBarbosaBrito@users.noreply.github.com>
2024-08-29 18:38:50 -0300

    Add files via upload

---
+++ MVC1/view/init.php
@@ -0,0 +1,11 @@
+<?php 
+
+    //definição do timezone
+
+    date_default_timezone_get('America/Sao Paulo');
+
+    //conexão para bd
+    define('BD_SERVIDOR','localhost');
+    define('BD_USUARIO','root');
+    define('BD_SENHA','senac');
+    define('BD_BANCO','livraria');
